e29e0326f80bc9d393c828b8562bb7f9755869f5
Bjørn Lindi <blindij@users.noreply.github.com>
2024-09-26 10:55:40 +0200

    Add source files.

---
+++ example_3/include/example_math.h
@@ -0,0 +1,11 @@
+/*
+ * The mathematical constants we use is listed here
+ */
+#define OUR_PI 3.14159265359
+#define OUR_E  2.718281828459
+
+/*
+ * The mathematical functions we have created is listed here
+ */
+
+double sin(double x);
---
+++ example_3/src/calculation.c
@@ -0,0 +1,11 @@
+/*
+ * This is our calculation
+ */
+#include "example_math.h"
+#include <stdio.h>
+
+int main(){
+   double d = OUR_PI/2;
+   printf("Sin(%f) = %f\n", d, sin(d));
+   return 0;
+}
